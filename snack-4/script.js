92c399154cf1a5c067edc3e37065097ceb394226
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-01-21 14:59:54 +0100

    feat: snack4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map((post) => {
+  return post.author;
+})
+
+console.log(authors);
