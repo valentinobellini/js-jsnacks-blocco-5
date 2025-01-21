ef973f57784aadbd820fa18b1fcd14b6e8f0f9c4
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-01-21 14:56:28 +0100

    feat: snack3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const additions = numbers.map((number) => {
+    return number + 1
+})
+
+console.log(additions);
